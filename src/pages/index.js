f57973aa2c78008d11b995c658b714eb9bbf3b77
Mick Jasinski <m.jasinski@minimalcraft.net>
2018-12-04 22:25:42 +0000

    Reset.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,9 @@
 import React from 'react'
 // import { Link } from 'gatsby'
 
-import homeImg  from '../images/homeArtwork.jpg';
-import stethoscope from '../images/stethoscope.svg';
-
-import Layout from '../components/layout'
 
 const IndexPage = () => (
-  <Layout>
-    <div>
-      <img src={homeImg} alt=""/>
-    </div>
-    <div>
-      <img src={stethoscope} alt="stethoscope" style={{
-        width: 160,
-      }}/>
-      <h3>Darmowa diagnoza sprzętu komputerowego. </h3>
-      <p>
-      Drobiazgowa diagnoza odbywa się w tym samym dniu. Kiedy sprzęt zostanie sprawdzony, skontaktujemy się w celu udzielenia szczegółowej informacji o usterce, kosztach naprawy i terminie realizacji. Następnie podejmą Państwo decyzję o przeprowadzeniu naprawy. W przypadku rezygnacji z naprawy nie ponoszą Państwo żadnych kosztów.
-      </p>
-    </div>
-
-  </Layout>
+  <div></div>
 )
 
 export default IndexPage
